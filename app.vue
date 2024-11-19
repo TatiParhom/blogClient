8a8d17699c67f90ddb361d655e5478616f0c886b
tatiana parkhomova <taniz2009@gmail.com>
2024-11-19 18:46:36 +0300

    *

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLoadingIndicator throttle="0" duration="5000" />
+  <Navbar />
+  <NuxtPage />
+  <Footer />
+  <Upp />
+</template>
+
+<script>
+const base = 'http://localhost:1337'
+const api = await $fetch(`${base}/api/config?populate=*`)
+const config = api.data
+
+useHead({
+  title: config.title,
+  meta: [
+    { name: 'description', content: config.desc },
+    { name: 'keywords', content: config.keywords },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: base + config.favicon.url },
+
+  ],
+  bodyAttrs: {
+    class: 'container mx-auto'
+  },
+})
+</script>
