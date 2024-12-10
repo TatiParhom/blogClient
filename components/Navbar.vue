cfa3a9b2a88aa2f04e59d07fbd76f9118635fd49
tatiana parkhomova <taniz2009@gmail.com>
2024-12-10 15:01:43 +0300

    search...

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,11 @@
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="http://e436b3cc88eb.vps.myjino.ru//" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://6bc41f582d60.vps.myjino.ru/uploads/cf2ff62cdf9411e29b1586b6a3b862cf_21bf27921f.png" class="h-8"
+    <div class="flex flex-wrap items-center justify-between mx-auto py-4">
+      <a href="https://615f68b9663f.vps.myjino.ru/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="https://a1c537287dd6.vps.myjino.ru/uploads/balanced_diet_686b9878eb.ico" class="h-8"
           alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Психология и
-          саморазвитие</span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">NuxtVerse</span>
       </a>
-      <!-- Поиск -->
-
-
       <div class="flex gap-8 md:order-2">
         <DarkMode />
         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false"
@@ -29,7 +25,7 @@
             </svg>
             <span class="sr-only">Search icon</span>
           </div>
-          <input type="text" id="search-navbar"
+          <input v-model="search.searchQuery" @focus="goToSearch" type="search" id="search-navbar"
             class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Поиск...">
         </div>
@@ -56,6 +52,19 @@
             class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Поиск...">
         </div>
+        <button id="theme-toggle" type="button"
+          class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+          <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+          </svg>
+          <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+              fill-rule="evenodd" clip-rule="evenodd"></path>
+          </svg>
+        </button>
         <ul
           class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
@@ -82,7 +91,6 @@
   </nav>
 </template>
 
-
 <script setup>
 const search = useSearchStore()
 
@@ -92,7 +100,7 @@ const isActive = (path) => route.path.split('/')[1] === path
 
 const router = useRouter()
 function goToSearch() {
-    router.push('/search')
+  router.push('/search')
 }
 </script>
 
